bd79551eabc0643d25b7bd6c529ce66ad5f0d1d4
HOT-POT1 <954905323@QQ.COM>
2021-02-10 12:54:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,12 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+git clone https://github.com/VPN-V2Ray/luci-theme-edge
+git clone https://github.com/jerrykuku/luci-app-argon-config
+git clone https://github.com/tindy2013/openwrt-subconverter
+git clone https://github.com/tuanqing/install-program package/install-program
+cd lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.gi
